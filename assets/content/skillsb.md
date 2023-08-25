b040cb703b5abc92651d11ca00f4c8786e981c3a
Jerry Hobby <Jerry@JerryHobby.com>
2023-08-25 10:13:16 -0500

    dynamic skills page

---
+++ assets/content/skillsb.md
@@ -0,0 +1,17 @@
+## Languages
+* C, C++, C#, JavaScript, TypeScript, Dart, Python, PHP
+* React, Flutter, Node.js, Express.js
+* SQL, NoSQL
+* Git, GitHub
+* HTML, CSS
+* Windows, Linux, Mac, iOS, Android
+## Tools
+* Visual Studio Code
+* Visual Studio
+* Android Studio
+* Xcode
+* Microsoft Office
+* Microsoft Project
+* Adobe Photoshop
+* Adobe Illustrator
+* Android Studio
--- lib/pages/skills.dart
+++ lib/pages/skills.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'components/app_utils.dart';
 
@@ -11,11 +12,14 @@ class Skills extends StatefulWidget {
   }
 }
 
+late EdgeInsets textPadding = EdgeInsets.fromLTRB(30, 10, 30, 0);
+
 class SkillsState extends State<Skills> {
   final pageTitle = 'Skills';
 
   late String content = "loading...";
-  late EdgeInsets textPadding = EdgeInsets.fromLTRB(30, 10, 30, 0);
+  late String contentA = "loading...";
+  late String contentB = "loading...";
 
   loadAsset(fileName) async {
     return rootBundle.loadString("assets/content/$fileName");
@@ -25,6 +29,16 @@ class SkillsState extends State<Skills> {
     loadAsset("skills.md").then((value) {
       setState(() {
         content = value;
+      });
+    });
+    loadAsset("skillsa.md").then((value) {
+      setState(() {
+        contentA = value;
+      });
+    });
+    loadAsset("skillsb.md").then((value) {
+      setState(() {
+        contentB = value;
       });
     });
   }
@@ -45,7 +59,80 @@ class SkillsState extends State<Skills> {
           alignment: Alignment.topLeft,
           child: markDown(content),
         ),
+        _buildLayout(context, pageTitle, contentA, contentB),
+        //return _buildListView(context, pageTitle, content);
       ],
     );
   }
 }
+
+_buildLayout(context, pageTitle, contentA, contentB) => LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        if (constraints.maxWidth > 600) {
+          return _buildWideContainers(
+              context, constraints, pageTitle, contentA, contentB);
+        } else {
+          return _buildNormalContainer(
+              context, constraints, pageTitle, contentA, contentB);
+        }
+      },
+    );
+
+_buildWideContainers(context, constraints, pageTitle, contentA, contentB) =>
+    Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            alignment: Alignment.topLeft,
+            padding: textPadding,
+            //width: constraints.maxWidth / 2,
+            width: 300,
+            child: markDown(contentA),
+          ),
+          Container(
+            alignment: Alignment.topLeft,
+            padding: textPadding,
+            //width: constraints.maxWidth / 2,
+            width: 300,
+            child: markDown(contentB),
+          ),
+        ],
+      ),
+    );
+
+_buildNormalContainer(context, constraints, pageTitle, contentA, contentB) =>
+    Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Container(
+            alignment: Alignment.topLeft, padding: textPadding,
+            child: markDown(contentA),
+          ),
+          Container(
+            alignment: Alignment.topLeft,
+            padding: textPadding,
+            child: markDown(contentB),
+          ),
+        ],
+      ),
+    );
+//
+// _buildListView(context, pageTitle, content) => ListView(
+//       children: [
+//         Center(
+//           child: SelectableText(
+//             pageTitle,
+//             style: Theme.of(context).textTheme.headlineMedium,
+//           ),
+//         ),
+//         Divider(),
+//         Container(
+//           padding: textPadding,
+//           alignment: Alignment.topLeft,
+//           child: markDown(content),
+//         ),
+//       ],
+//     );
